e1bab9c0bb3b7c6a21c453dab880749ff88e67d4
Nícolas Ferri <162733007+KingNico06@users.noreply.github.com>
2025-04-22 22:24:49 -0300

    Exercício 2

---
+++ src/exercicios/Exercicio2.java
@@ -0,0 +1,37 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Quantos números você deseja inserir? ");
+        int quantidade = scanner.nextInt();
+        
+        int[] numeros = new int[quantidade];
+        int soma = 0;
+
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+            soma += numeros[i];
+        }
+
+
+        double media = (double) soma / quantidade;
+
+
+        System.out.println("\nLista de números inseridos:");
+        for (int numero : numeros) {
+            System.out.print(numero + " ");
+        }
+        
+        System.out.println("\n\nSoma: " + soma);
+        System.out.println("\nMédia: " + media);
+
+        scanner.close();
+    }
+}
